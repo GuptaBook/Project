6802b6e0f60c161063ecb84daa7acd74f821028e
sirishaganta <43954160+sirishaganta@users.noreply.github.com>
2018-10-25 19:44:06 +0530

    History pojo

---
+++ OnlineGuptaBookStore/src/com/thirdware/guptabookstore/models/history.java
@@ -0,0 +1,62 @@
+package com.thirdware.guptabookstore.models;
+
+public class history {
+	private int id;
+	private String bookname;
+	private int quantity;
+	private float price;
+	private int cid;
+	private int bookid;
+	private String custemail;
+	private String hisdate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public int getBookid() {
+		return bookid;
+	}
+	public void setBookid(int bookid) {
+		this.bookid = bookid;
+	}
+	public String getCustemail() {
+		return custemail;
+	}
+	public void setCustemail(String custemail) {
+		this.custemail = custemail;
+	}
+	public String getHisdate() {
+		return hisdate;
+	}
+	public void setHisdate(String hisdate) {
+		this.hisdate = hisdate;
+	}
+	
+
+}
